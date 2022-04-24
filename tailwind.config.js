b2582a06367a935eed7d25d2ce6bb4b0874cbac3
Antonio Lopez <pezlop202@gmail.com>
2022-04-24 15:38:20 -0700

    created grid container and styled top grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         brightYellow: 'hsl(71, 73%, 54%)',
         lightGray: 'hsl(204, 43%, 93%)',
         grayishBlue: 'hsl(218, 22%, 67%)',
+        offCyan: 'hsla(179, 62%, 43%, 0.699)',
       },
       fontFamily: {
         'sans-serif': ['Karla'],
